230efcfd400ebae3c6a7169362c1539a7a3d454b
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2025-01-23 16:41:55 -0300

    feat: fastify setup

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,11 +1,33 @@
 import fastifyCors from '@fastify/cors'
 import fastify from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, _request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+  // observability tool
+  console.error(error)
+  return reply.status(500).send({ message: 'Internal server error' })
+})
 
 server.register(fastifyCors, {
   origin: '*',
 })
+
+server.register(uploadImageRoute)
 
 server.listen({ port: 3000, host: '0.0.0.0' }).then(() => {
   console.log('Server is running on port 3000')
